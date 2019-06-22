d88eef3750d70dbe48fa43f69814e76b1d16b559
Huan LI <zixia@zixia.net>
2019-06-22 12:48:31 +0800

    follow chatie devops toolset

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env ts-node
 
-// tslint:disable:no-console
-
 import {
   SelfIdResponse,
   VERSION,
 }           from '@chatie/grpc'
 
 async function main () {
+  if (VERSION === '0.0.0') {
+    throw new Error('version should be set before publishing')
+  }
+
   const selfIdResponse = new SelfIdResponse()
   selfIdResponse.setId('id')
-  console.log(`@chatie/grpc v${VERSION} smoking test passed.`)
+  console.info(`@chatie/grpc v${VERSION} smoking test passed.`)
   return 0
 }
 
 main()
-.then(process.exit)
-.catch(e => {
-  console.error(e)
-  process.exit(1)
-})
+  .then(process.exit)
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
