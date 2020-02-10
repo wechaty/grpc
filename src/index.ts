25860d53238aa37ffb1790fc154c623f024e8e7a
Huan LI <zixia@zixia.net>
2020-02-10 09:50:14 +0800

    Rename proto files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from '../generated/wechaty-puppet/base_pb'
 export * from '../generated/wechaty-puppet/contact_pb'
 export * from '../generated/wechaty-puppet/friendship_pb'
 export * from '../generated/wechaty-puppet/message_pb'
-export * from '../generated/wechaty-puppet/room-invitation_pb'
+export * from '../generated/wechaty-puppet/room_invitation_pb'
 export * from '../generated/wechaty-puppet/room_pb'
 
 export * from '../generated/wechaty-puppet_grpc_pb'
