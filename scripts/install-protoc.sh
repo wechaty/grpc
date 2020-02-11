4cb5d2ea37d36609752f21b6b3220982df2a711b
Huan LI <zixia@zixia.net>
2020-02-11 14:37:18 +0800

    fix chmod -R

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -20,7 +20,7 @@ PROTOC_ZIP="protoc-$PROTOC_VERSION-$PROTOC_PLATFORM-x86_64.zip"
 curl -OL "https://github.com/google/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP"
 # See: https://github.com/grpc-ecosystem/grpc-gateway/issues/194
 sudo unzip -o $PROTOC_ZIP -d /usr/local bin/* include/*
-sudo chmod 755 /usr/local/include/google/
+sudo chmod -R 755 /usr/local/include/google/
 sudo chmod +x /usr/local/bin/protoc
 rm -f $PROTOC_ZIP
 
@@ -33,4 +33,4 @@ PROTOC_GEN_LINT_ZIP="protoc-gen-lint_${PROTOC_GEN_LINT_PLATFORM}_amd64.zip"
 curl -OL "https://github.com/ckaznocha/protoc-gen-lint/releases/download/v$PROTOC_GEN_LINT_VERSION/$PROTOC_GEN_LINT_ZIP"
 sudo unzip -o "$PROTOC_GEN_LINT_ZIP" protoc-gen-lint -d /usr/local/bin protoc-gen-lint
 sudo chmod +x /usr/local/bin/protoc-gen-lint
-rm -f "PROTOC_GEN_LINT_ZIP"
+rm -f "$PROTOC_GEN_LINT_ZIP"
