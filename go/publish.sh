3bb8bd2427f0482234aa3ee5bd4c63d7711204f7
Huan LI <zixia@zixia.net>
2020-04-13 22:19:50 +0800

    Deploy to go repo

--- go/publish.sh
+++ go/publish.sh
@@ -8,9 +8,8 @@ VERSION=$(jq -r .version ../package.json)
 DEPLOY_DIR="wechaty-go-grpc.$$"
 
 mkdir "$DEPLOY_DIR"
-# trap "rm -rfv $(pwd)/$DEPLOY_DIR" EXIT
-
 pushd "$DEPLOY_DIR"
+trap "rm -rfv $(pwd)/$DEPLOY_DIR" EXIT
 
 git clone git@github.com:wechaty/go-grpc.git
 cd go-grpc
@@ -19,12 +18,22 @@ echo "$VERSION" > VERSION
 
 if [ -z "$(git status --porcelain)" ]; then
   echo
-  echo 'Working directory clean'
+  echo "[Publish] There's no new generated code found"
   echo
-else
-  git commit -am "Deploy Go Grpc Module v${VERSION}"
-  git push
-  git status
+  exit 0
 fi
 
+git commit -am "Deploy Go Grpc Module v${VERSION}"
+git push
+echo
+echo '[Publish] New code has been generated and pushed'
+echo
+
+git tag v"$VERSION"
+git push origin v"$VERSION"
+echo
+echo "[Publish] New version ${VERSION} has been tagged"
+echo
+
+git status
 popd
