da11bdfedb0dfaa5c43b5f6cd2d93d458d05fd56
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-10 15:07:37 +0800

    add php grpc

---
+++ php/grpc_test.php
@@ -0,0 +1,19 @@
+<?php
+define("ROOT", __DIR__);
+
+function autoload($clazz) {
+    $file = str_replace('\\', '/', $clazz);
+    if(is_file(ROOT . "/generated/wechaty/$file.php")) {
+        require ROOT . "/generated/wechaty/$file.php";
+    } else {
+        $file = str_replace('\\', '/', $clazz);
+        if(is_file("/usr/share/pear/$file.php")) {
+            require "/usr/share/pear/$file.php";
+        }
+    }
+}
+
+spl_autoload_register("autoload");
+
+$client = new \Wechaty\PuppetClient("localhost", array());
+$client->Version(null);
