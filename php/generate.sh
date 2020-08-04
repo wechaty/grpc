7ade7c5bbe53e880e5550cde8746fc8a65d36695
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-04 22:38:18 +0800

    add php action

--- php/generate.sh
+++ php/generate.sh
@@ -6,6 +6,8 @@ PROTO_BASE_DIR=../proto
 PROTO_PUPPET_DIR=$PROTO_BASE_DIR/wechaty/puppet
 PROTO_WECHATY_DIR=$PROTO_BASE_DIR/wechaty
 
+# /usr/local/include
+
 OUT_WECHATY_DIR=./generated/wechaty
 OUT_PUPPET_DIR=$OUT_WECHATY_DIR
 
