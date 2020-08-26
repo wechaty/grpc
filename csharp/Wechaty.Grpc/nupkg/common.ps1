1d1de76c81c5d0c8342c0e163c43d4af93a2a7b9
zhengbo <zhengbo@7net.cc>
2020-08-26 23:27:29 +0800

    update

--- csharp/Wechaty.Grpc/nupkg/common.ps1
+++ csharp/Wechaty.Grpc/nupkg/common.ps1
@@ -4,3 +4,9 @@ $packFolder = (Get-Item -Path "./" -Verbose).FullName
 $rootFolder = Join-Path $packFolder "../../../"
 
 echo $packFolder
+
+
+https://www.jb51.net/article/53528.htm
+https://www.pstips.net/powershell-replace-text.html
+https://www.jb51.net/article/32470.htm
+https://www.jb51.net/article/53528.htm
