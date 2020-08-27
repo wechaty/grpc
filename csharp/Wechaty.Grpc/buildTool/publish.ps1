efb4d90db13415a7c0bb8c2d54fccd4b93895c5d
Darren <jesn2013@hotmail.com>
2020-08-27 15:16:35 +0800

    test  action args key

--- csharp/Wechaty.Grpc/buildTool/publish.ps1
+++ csharp/Wechaty.Grpc/buildTool/publish.ps1
@@ -2,12 +2,13 @@
 
 $apiKey = $args[0]
 
+echo "api-key"
 echo $apiKey
 
 Write-Output $apiKey;
 
-$packageFile=Join-Path $packageFolder ("Wechaty.Grpc."+ $version + ".nupkg")
+#$packageFile=Join-Path $packageFolder ("Wechaty.Grpc."+ $version + ".nupkg")
 
-dotnet nuget push $packageFile -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
+#dotnet nuget push $packageFile -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 
 Set-Location $buildFolder
