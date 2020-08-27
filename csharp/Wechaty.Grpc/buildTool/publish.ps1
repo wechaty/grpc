dfe46184fed7f508b089ef440ee7ff5c690149b7
Darren <jesn2013@hotmail.com>
2020-08-27 15:38:45 +0800

    update yaml

--- csharp/Wechaty.Grpc/buildTool/publish.ps1
+++ csharp/Wechaty.Grpc/buildTool/publish.ps1
@@ -2,14 +2,8 @@
 
 $apiKey = $args[0]
 
-echo "api-key"
-echo $apiKey
+$packageFile=Join-Path $packageFolder ("Wechaty.Grpc."+ $version + ".nupkg")
 
-Write-Output $apiKey;
-
-
-#$packageFile=Join-Path $packageFolder ("Wechaty.Grpc."+ $version + ".nupkg")
-
-#dotnet nuget push $packageFile -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
+dotnet nuget push $packageFile -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 
 Set-Location $buildFolder
