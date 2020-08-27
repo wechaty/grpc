67fadae89b5aacf45945e632d0b8e8000049d1e5
Darren <jesn2013@hotmail.com>
2020-08-27 15:24:30 +0800

    test github Secrets

--- csharp/Wechaty.Grpc/buildTool/publish.ps1
+++ csharp/Wechaty.Grpc/buildTool/publish.ps1
@@ -7,6 +7,7 @@ echo $apiKey
 
 Write-Output $apiKey;
 
+
 #$packageFile=Join-Path $packageFolder ("Wechaty.Grpc."+ $version + ".nupkg")
 
 #dotnet nuget push $packageFile -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
