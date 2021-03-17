ab3c529163b61b5061bbcb0b683cf86a9aea52bb
Huan LI (李卓桓) <zixia@zixia.net>
2021-03-18 00:11:40 +0800

    show version when install grpc-gateway (#109)

--- openapi/install.sh
+++ openapi/install.sh
@@ -8,6 +8,7 @@ THIRD_PARTY_DIR="${SCRIPTPATH}/../third-party/"
 
 function go_install () {
   go install \
+    github.com/grpc-ecosystem/grpc-gateway/v2 \
     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
     google.golang.org/protobuf/cmd/protoc-gen-go \
