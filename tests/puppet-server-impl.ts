05345c49830e6a9df94966c13c847cbf591a8e61
su-chang <su_chang@yeah.net>
2021-04-08 12:01:27 +0800

    fix: add conversationRead method in tests

--- tests/puppet-server-impl.ts
+++ tests/puppet-server-impl.ts
@@ -121,6 +121,12 @@ export const puppetServerImpl: IPuppetServer = {
     throw new Error('not implemented.')
   },
 
+  conversationRead: (call, callback) => {
+    void call
+    void callback
+    throw new Error('not implemented.')
+  },
+
   messageContact: (call, callback) => {
     void call
     void callback
