a3dbf69e9be6bce7e140b12f5f29af0cf75def0d
Huan <zixia@zixia.net>
2021-08-30 17:44:38 +0800

    wip

--- src/mod.ts
+++ src/mod.ts
@@ -1,16 +1,28 @@
-import * as proto from './proto'
+import * as grpc    from '@grpc/grpc-js'
+import * as puppet  from './generated'
+
+import {
+  StringValue,
+}               from 'google-protobuf/google/protobuf/wrappers_pb'
+
+import * as proto   from './proto'
 import * as openApi from './openapi'
 import {
   VERSION,
 }           from './config'
 
+/**
+ * https://stackoverflow.com/a/67697999/1123955
+ *  Typescript class variables and error:
+ *    “refers to a value, but is being used as a type here.
+ *    Did you mean typeof' …”. Why exactly?
+ */
+
 export {
-  proto,
+  grpc,
   openApi,
+  proto,
+  puppet,
+  StringValue,
   VERSION,
 }
-export * as grpc    from '@grpc/grpc-js'
-export * as puppet  from './generated'
-export {
-  StringValue,
-}                 from 'google-protobuf/google/protobuf/wrappers_pb'
