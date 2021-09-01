139a78176fe488dbecfc5500d445100947176522
Huan <zixia@zixia.net>
2021-09-02 03:00:29 +0800

    rename to commonjs/

--- src/cjs.ts
+++ src/cjs.ts
@@ -1,5 +1,5 @@
-import codeRootPkg from '../cjs/code-root.js'
-import { puppet } from '../cjs/puppet.js'
+import codeRootPkg from '../commonjs/code-root.js.js'
+import { puppet } from '../commonjs/puppet.js.js'
 
 const codeRoot = codeRootPkg['codeRoot']
 // const puppet = puppetPkg.puppet
