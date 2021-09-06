e44057577aea4199743bbb2f3ca7c3c609bb29e6
Huan LI (李卓桓) <zixia@zixia.net>
2021-09-07 00:13:20 +0800

    clean

--- src/cjs.ts
+++ src/cjs.ts
@@ -1,6 +1,6 @@
 // import codeRootPkg from '../commonjs/code-root.js'
 import { codeRoot } from '../commonjs/code-root.js'
-import { puppet } from '../commonjs/puppet.js'
+import { puppet }   from '../commonjs/puppet.js'
 
 // const codeRoot = codeRootPkg['codeRoot']
 // const puppet = puppetPkg.puppet
