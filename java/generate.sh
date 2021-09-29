1d05fb60a96f6fbbcd0f4866c30d26bdff321864
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-29 15:25:11 +0800

    add java generate

--- java/generate.sh
+++ java/generate.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 PROTO_BASE_DIR=../proto
+THIRD_PARTY_DIR=../third-party
 PROTO_PUPPET_DIR=$PROTO_BASE_DIR/wechaty/puppet
 PROTO_WECHATY_DIR=$PROTO_BASE_DIR/wechaty
 
@@ -19,10 +20,12 @@ protoc --version
 
 protoc \
   -I $PROTO_BASE_DIR \
+  -I $THIRD_PARTY_DIR \
   --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java-1.37.1-linux-x86_64.exe \
   --grpc-java_out="$OUT_PUPPET_DIR" $PROTO_PUPPET_DIR/*.proto
 
 protoc \
   -I $PROTO_BASE_DIR \
+  -I $THIRD_PARTY_DIR \
   --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java-1.37.1-linux-x86_64.exe \
   --grpc-java_out="$OUT_PUPPET_DIR" $PROTO_WECHATY_DIR/*.proto
