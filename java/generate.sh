9c1fae9315d462682b5fd9a71e3c701fc46101a8
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-29 16:02:48 +0800

    add java generate

--- java/generate.sh
+++ java/generate.sh
@@ -22,10 +22,14 @@ protoc \
   -I $PROTO_BASE_DIR \
   -I $THIRD_PARTY_DIR \
   --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java-1.37.1-linux-x86_64.exe \
-  --grpc-java_out="$OUT_PUPPET_DIR" $PROTO_PUPPET_DIR/*.proto
+  --java_out=$OUT_PUPPET_DIR \
+  --grpc_out=$OUT_PUPPET_DIR \
+  $PROTO_PUPPET_DIR/*.proto
 
 protoc \
   -I $PROTO_BASE_DIR \
   -I $THIRD_PARTY_DIR \
   --plugin=protoc-gen-grpc-java=/usr/local/bin/protoc-gen-grpc-java-1.37.1-linux-x86_64.exe \
-  --grpc-java_out="$OUT_PUPPET_DIR" $PROTO_WECHATY_DIR/*.proto
+  --java_out=$OUT_PUPPET_DIR \
+  --grpc_out=$OUT_PUPPET_DIR \
+  $PROTO_WECHATY_DIR/*.proto
