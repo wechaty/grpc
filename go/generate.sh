e105aeff6f0d2d4efff8e0cf27139707db9e3d7e
小雨 <614422099@QQ.com>
2021-10-04 02:54:43 +0800

    Update generate.sh

--- go/generate.sh
+++ go/generate.sh
@@ -22,6 +22,5 @@ protoc \
   --go_opt=paths=source_relative \
   --go-grpc_out=$OUT_DIR \
   --go-grpc_opt=paths=source_relative \
-  "$ROOT/proto/wechaty"/puppet.proto \
   "$ROOT/proto/wechaty"/**/*.proto \
   "$ROOT/third-party"/**/*.proto
