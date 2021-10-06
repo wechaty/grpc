9769a4a5e605a547d045a20a20f1cf65b2ed9af5
Huan <zixia@zixia.net>
2021-10-07 01:01:06 +0800

    clean

--- src/cjs.ts
+++ src/cjs.ts
@@ -1,9 +1,5 @@
-// import codeRootPkg from '../commonjs/code-root.js'
 import { codeRoot } from '../commonjs/code-root.cjs'
 import { puppet }   from '../commonjs/puppet.cjs'
-
-// const codeRoot = codeRootPkg['codeRoot']
-// const puppet = puppetPkg.puppet
 
 export {
   codeRoot,
